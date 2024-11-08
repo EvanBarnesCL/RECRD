1d989997ca90b322b77c2c8d18a4798cce98f039
EvanBarnesCL <156237818+EvanBarnesCL@users.noreply.github.com>
2024-11-08 15:51:03 -0800

    custom drv8835 library integrated

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,77 @@
 #include <Arduino.h>
 #include <Ramp.h>
 #include <AnalogEncoder.h>
-#include <Adafruit_APDS9960.h>
-#include <SPI.h>
-// proxy values for clockwise and ccw rotation of table
-#define CCW   HIGH
-#define CW    LOW
+#include <digitalWriteFast.h>
+#include <Crunchlabs_DRV8835.h>
 
-constexpr int TABLE_SPEED_PIN = 6;
-constexpr int TABLE_DIR_PIN = 7;
-constexpr int ARM_SPEED_PIN = 5;
-constexpr int ARM_DIR_PIN = 4;
+constexpr uint8_t CW = 1;
+constexpr uint8_t CCW = -1;
+
+constexpr uint8_t ARM_OUT = 0;
+constexpr uint8_t ARM_IN = 1;
+
+constexpr uint8_t TABLE_SPEED_PIN = 6;
+constexpr uint8_t TABLE_DIR_PIN = 7;
+constexpr uint8_t ARM_SPEED_PIN = 5;
+constexpr uint8_t ARM_DIR_PIN = 4;
+
+constexpr uint8_t ARM_ENC_PIN = A6;
+constexpr uint8_t TABLE_ENC_PIN = A7;
 
 constexpr int HYST_OFFSET = 10;
 
 constexpr int ARM_MOTOR_MIN_SPEED = 150;      // in testing, this was the lowest PWM value that would reliably move the arm to the demanded position without occasional stalls.
 
-void setMotorSpeed(int speedPin, int dirPin, int speed);
+
+// temporary function for setting motor speed. make this a class later.
+void setMotorSpeed(uint8_t speedPin, uint8_t dirPin, int8_t speed, uint8_t minPWM = 0);
 
 int target = 0, targetDeg = 0;  // in encoder counts, in degrees
 
+constexpr uint8_t MIN_ARM_PWM = 80;
 
-rampInt positionRamp;
+// rampInt positionRamp;
 
 
-AnalogEncoder tableEncoder(A0);
-AnalogEncoder armEncoder(A1, true);
+AnalogEncoder tableEncoder(TABLE_ENC_PIN);
+AnalogEncoder armEncoder(ARM_ENC_PIN);
 
-Adafruit_APDS9960 colorSensor;
+
+DRV8835 tableMotor(TABLE_SPEED_PIN, TABLE_DIR_PIN, 85, true);
 
 
 void setup()
 {
   Serial.begin(115200);
   Serial.println("starting");
-  Wire.begin();
 
-  pinMode(TABLE_DIR_PIN, OUTPUT);
-  pinMode(TABLE_SPEED_PIN, OUTPUT);
-  pinMode(ARM_DIR_PIN, OUTPUT);
-  pinMode(ARM_SPEED_PIN, OUTPUT);
+  
 
-  if(!colorSensor.begin()){
-    Serial.println("failed to initialize device! Please check your wiring.");
+  while (true) {
+    int speed = 1;
+    tableMotor.setSpeed(speed);
+    Serial.print(speed); Serial.print("    "); Serial.println(tableMotor.getspeedActual());
+    delay(10000);
+    speed = -1;
+    tableMotor.setSpeed(speed);
+    Serial.print(speed); Serial.print("    "); Serial.println(tableMotor.getspeedActual());
+    delay(2000);
   }
-  else Serial.println("Device initialized!");
-  delay(1000);
-  colorSensor.enableColor(true);
 
-  digitalWrite(ARM_DIR_PIN, HIGH);
-  analogWrite(ARM_SPEED_PIN, 200);
-  delay(1000);
-  analogWrite(ARM_SPEED_PIN, 0);
+  // pinMode(TABLE_DIR_PIN, OUTPUT);
+  // pinMode(TABLE_SPEED_PIN, OUTPUT);
+  // pinMode(ARM_DIR_PIN, OUTPUT);
+  // pinMode(ARM_SPEED_PIN, OUTPUT);
+
+
+  // digitalWrite(ARM_DIR_PIN, ARM_OUT);
+  // analogWrite(ARM_SPEED_PIN, 200);
+  // delay(3000);
+  // analogWrite(ARM_SPEED_PIN, 0);
+
+  setMotorSpeed(ARM_SPEED_PIN, ARM_DIR_PIN, 255, 85);
+  delay(8000);
+  setMotorSpeed(ARM_SPEED_PIN, ARM_DIR_PIN, 0, 85);
 
   armEncoder.begin();
   tableEncoder.begin();
@@ -60,53 +80,70 @@ void setup()
 
   delay(2000);
 
-  positionRamp.go(512, 20000, LINEAR, FORTHANDBACK);
-  target = 0;
+  // positionRamp.go(512, 20000, LINEAR, FORTHANDBACK);
+  // target = 0;
 
 
   // start the table spinning just for fun
-  digitalWrite(TABLE_DIR_PIN, CW);
-  analogWrite(TABLE_SPEED_PIN, 80);
+  // digitalWrite(TABLE_DIR_PIN, CW);
+  // analogWrite(TABLE_SPEED_PIN, 80);
+
+  setMotorSpeed(TABLE_SPEED_PIN, TABLE_DIR_PIN, 1 * CW, 90);
 }
 
 
 void loop() {
-    static unsigned long lastPrint = millis();
-    static unsigned long lastColorUpdate = millis();
-    uint16_t r, g, b, c;
-    target = positionRamp.update();
-    int currentPos = armEncoder.read();
-    int error = target - currentPos;
-    int mSpeed = (int)(1.0 * error);
-    if (error > 5) {
-      setMotorSpeed(ARM_SPEED_PIN, ARM_DIR_PIN, -ARM_MOTOR_MIN_SPEED);
-    } else if (error < -5) {
-      setMotorSpeed(ARM_SPEED_PIN, ARM_DIR_PIN, ARM_MOTOR_MIN_SPEED);
-    } else {
-      setMotorSpeed(ARM_SPEED_PIN, ARM_DIR_PIN, 0);
+  static unsigned long lastPrint = millis();
+  static unsigned long lastColorUpdate = millis();
+  uint16_t r, g, b, c;
 
-    if (millis() - lastPrint > 100) {
-      if (colorSensor.colorDataReady()) colorSensor.getColorData(&r, &g, &b, &c);
-      String str = "R: " + String(r) + "    G: " + String(g) + "    B: " + String(b) + "    C: " + String(c);
-      int tPos = tableEncoder.read();
-      int aPos = armEncoder.read();
-      str += "    Arm: " + String(aPos) + "    Table: " + String(tPos);
-      Serial.println(str);
-      lastPrint = millis();
-    }
-
+  if (millis() - lastPrint > 100) {
+    Serial.println(tableEncoder.read());
+    lastPrint = millis();
   }
+
+
+  // target = positionRamp.update();
+  // int currentPos = armEncoder.read();
+  // int error = target - currentPos;
+  // int mSpeed = (int)(1.0 * error);
+  // if (error > 5) {
+  //   setMotorSpeed(ARM_SPEED_PIN, ARM_DIR_PIN, -ARM_MOTOR_MIN_SPEED);
+  // } else if (error < -5) {
+  //   setMotorSpeed(ARM_SPEED_PIN, ARM_DIR_PIN, ARM_MOTOR_MIN_SPEED);
+  // } else {
+  //   setMotorSpeed(ARM_SPEED_PIN, ARM_DIR_PIN, 0);
+
+  //   if (millis() - lastPrint > 100) {
+  //     if (colorSensor.colorDataReady()) colorSensor.getColorData(&r, &g, &b, &c);
+  //     String str = "R: " + String(r) + "    G: " + String(g) + "    B: " + String(b) + "    C: " + String(c);
+  //     int tPos = tableEncoder.read();
+  //     int aPos = armEncoder.read();
+  //     str += "    Arm: " + String(aPos) + "    Table: " + String(tPos);
+  //     Serial.println(str);
+  //     lastPrint = millis();
+  //   }
+
+  // }
 }
 
-void setMotorSpeed(int speedPin, int dirPin, int speed) {
-  if (speed < 0) {
+void setMotorSpeed(uint8_t speedPin, uint8_t dirPin, int8_t speed, uint8_t minPWM = 0) {
+  uint8_t _speed;
+  if (speed == 0) {
+    analogWrite(speedPin, 0);
+  } else if (speed < 0) {
     // Reverse direction
-    digitalWrite(dirPin, LOW);
-    analogWrite(speedPin, -speed); // Use absolute value for speed
+    digitalWrite(dirPin, ARM_OUT);
+    _speed = abs(speed);
+    _speed = map(_speed, 0, 255, minPWM, 255);
+    analogWrite(speedPin, _speed); // Use absolute value for speed
   } else {
     // Forward direction
-    digitalWrite(dirPin, HIGH);
-    analogWrite(speedPin, speed);
+    digitalWrite(dirPin, ARM_IN);
+    _speed = abs(speed);
+    _speed = map(_speed, 0, 255, minPWM, 255);
+    analogWrite(speedPin, _speed);
   }
+  Serial.println(_speed);
 }
 
