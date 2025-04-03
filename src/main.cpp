46ab2eeb7701f3c6b049705fd96d50bf045da261
EvanBarnesCL <156237818+EvanBarnesCL@users.noreply.github.com>
2025-04-02 18:17:58 -0700

    added arm angle and radius conversion functions

--- src/main.cpp
+++ src/main.cpp
@@ -169,8 +169,12 @@ void homeArm();
 bool debounceSwitch();
 
 
-int16_t armDistanceToAngle(int16_t distance);     // calculates arm encoder angle from linear position of color sensor
-int16_t armAngleToDistance(int16_t angle);        // calculates linear position of color sensor based on arm encoder angle
+int16_t armRadiusToAngle(int16_t radiusMM);     // convert arm radius in millimeters to angle in encoder counts
+int16_t armAngleToRadius(int16_t angleCounts);  // convert arm angle in encoder counts to radius in millimeters
+
+
+// int16_t armDistanceToAngle(int16_t distance);     // calculates arm encoder angle from linear position of color sensor
+// int16_t armAngleToDistance(int16_t angle);        // calculates linear position of color sensor based on arm encoder angle
 bool initializationRoutine();                     // initialization routine to properly position the arm over the table and prime the color sensor
 uint8_t noteNameToMIDINote(const char* noteName);          // convert note names to MIDI note numbers (e.g., F#2 -> 42)
 const char* MIDINoteToNoteName(uint8_t note);     // convert MIDI note to note name (e.g., 42 -> F#2)
@@ -625,8 +629,81 @@ void homeArm() {
   armEncoder.resetCumulativePosition();
 }
 
+/**
+ * @brief converts an arm radius (from center of table in millimeters) to the angle in encoder counts that the arm needs to be at to reach that radius.
+ * 
+ * @param radiusMM the radius in millimeters
+ * 
+ * @return the angle in encoder counts that corresponds to the input radius
+ * 
+ * @paragraph The angle is defined from the line that connects the center of the table and the center of the arm gear. As the angle
+ * gets more positive, the arm moves toward its vertical position. Negative angles move the other side of the center
+ * line, bringing the sensor arm down toward the knobs and buttons. 
+ * 
+ * This function is an approximation, but a pretty good one. At worst, it will have an error of 1 degree. The actual function that
+ * precisely converts radius to angle in encoder counts (4096 per revolution) is: 
+ * 
+ * angle = (4096 / pi) * arcsin(radiusMM / 216)
+ * 
+ * This radius is actually the chord length of the arc described by the sensor arm, starting from the point where that arc is
+ * coincident with the table. The above function is derived from the formula for chord length. It's nice because it's exact, but it's
+ * terrible for use in this context because it has floating point numbers (pi) and trig functions (arcsine). The ATMega328P is 
+ * extremely slow at performing floating point operations and trigonometric functions. It only has hardware for adding, subtracting,
+ * and multiplying integers, as well as hardware for performing bitwise operations directly on binary numbers, so if you need any 
+ * other math operation, it gets built from those basic operations repeated a bunch of times, making it really slow. In situations
+ * like this, where we need as much processing power as possible for running Mozzi, it's worth trying to find good linear approximations
+ * of the actual math function that only use those basic operations. The approximation I derived is: 
+ * 
+ * angle = (774 * radiusMM) >> 7;
+ * 
+ * That's equivalent to (774 * radiusMM) / 128. The denominator of 128 is nice because that's 2^7, so instead of performing the slow
+ * operation of dividing by 128, we can perform the extremely fast operation of bitshifting right by 7 places. 
+ * 
+ * If you want you can see a spreadsheet that I made to help figure all this out. It's not particularly well organized, and it's missing
+ * the original math that I did by hand, as well as the Desmos graphs I made to check my reasoning, but it should help get some of the 
+ * concepts clarified. https://docs.google.com/spreadsheets/d/1RaxqJCClSnBzAPjxKA0sKCvFVXpXQ3Gc1AZQWVGnURM/edit?usp=sharing
+ */
+int16_t armRadiusToAngle(int16_t radiusMM) {
+  return (774 * radiusMM) >> 7;
+}
 
 
+/**
+ * @brief converts an arm angle in encoder counts to the corresponding radius of the sensor from the center of the table in millimeters. 
+ * 
+ * @param angleCounts the angle in encoder counts (4096 per revolution). 
+ * 
+ * @return the radius of the sensor from the center of the table in millimeters. 
+ * 
+ * @paragraph This is similar to the function armRadiusToAngle() in which I used a linear integer approximation of a function that
+ * relies on floating point numbers and trigonometry functions. The precise function that converts an angle to a radius is: 
+ * 
+ * radius = 216 * sin(angle * pi / 4096)
+ * 
+ * This can be pretty well approximated by the function
+ * 
+ * radius = 81 * angle >> 9
+ * 
+ * At worst, this approximation will produce a positional error of 2mm. This seems acceptable in this context, since backlash in the gears
+ * and play in the arm height adjustment system produce more that 2mm of error, and this isn't a system where 2mm of error is going to make
+ * a huge difference. The average and median error are .17mm and .42mm respectively in my sample set, which you can see in the spreadsheet 
+ * linked above.
+ * 
+ * One important note about the linear approximations in both of these functions is that they only work because we're working with a restricted
+ * domain. The sensor arm can only move through limited range of motion from about -44 degrees to 58 degrees. The functions that convert
+ * angle to radius and radius to angle both look reasonably linear across that domain. However, if we were dealing with an unrestricted range
+ * of motion (if the arm could spin in a full circle), these approximations wouldn't work at all, and we would need to look into other methods
+ * of speeding up these math operations. If you're interested in this, you could look into piecewise linear functions and the closely related
+ * concept of a lookup table with linear interpolation. You could also look into CORDIC (coordinate rotation digital computer) which is a common
+ * efficient method for calculating things like trig functions. This kind of thing is used everywhere in software. Every video game you've ever
+ * played has basically been a giant system of linear algebra and trig functions. 
+ */
+int16_t armAngleToRadius(int16_t angleCounts) {
+  return 81 * angleCounts >> 9;
+}
+
+
+/*
 int16_t armDistanceToAngle(int16_t distance) {
   // This is only accurate for input distances ranging from 0 to 100mm.
   // Beyond this point the mechanism itself is non-linear.
@@ -676,6 +753,7 @@ int16_t armAngleToDistance(int16_t angle) {
   return (int16_t)distance;
 }
 
+*/
 
 bool updateColorReadings(ColorValues *colorReadings) {
   RGBCIR.readRGBWIR(colorReadings->red, colorReadings->green, colorReadings->blue, colorReadings->clear, colorReadings->IR);
