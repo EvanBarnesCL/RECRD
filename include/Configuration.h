7284171080fc2a080f26e82905bdc6b37e70d21d
EvanBarnesCL <156237818+EvanBarnesCL@users.noreply.github.com>
2026-03-31 10:57:11 -0700

    updated configuration.h

--- include/Configuration.h
+++ include/Configuration.h
@@ -4,16 +4,16 @@
 #include <Mozzi.h>
 
 // Easy way to toggle Serial print statements. See Debug.h to see how this works.
-// Uncomment the following line to enable serial print debugging:
+// Set the following value to 1 to enable serial print debugging:
 
-// #define USE_SERIAL           
+#define USE_SERIAL 0          // options: 0 or 1 (0 default)
 #include <Debug.h>
 
 // set to 1 to change the PWM clock divisor for pins 3 and 11 (timer 2) - removes PWM noise due to LED dimming from audio
-#define USE_LED_PWM 1 
+#define USE_LED_PWM 1         // options: 1 or 0 (1 default) 
 
 // set to 1 to change the PWM clock divisor for pins 5 and 6 (timer 0) - removes motor noise from audio
-#define USE_FAST_PWM 1
+#define USE_FAST_PWM 1        // options: 1 or 0 (1 default)
 
 // defines how often to update one of the sensors. The sensors get updated one at a time, so if this is left as the default
 // 15ms interval, each sensor will be updated every 45ms.
@@ -28,7 +28,7 @@ constexpr uint8_t I2C_UPDATE_INTERVAL = 15; // time in milliseconds
 // Higher values improve responsiveness to sensor input but reduce processing time available for audio synthesis. 
 // Audio glitching may indicate this value should be reduced.
 
-#define MOZZI_CONTROL_RATE 128    
+#define MOZZI_CONTROL_RATE 128    // options: 8, 16, 32, 64, 128, 256, 512 (default 128)
 
 // These are the wavetables that will be used for each oscillator voice. Mozzi offers a lot of them to try out!
 // To load a new wavetable that isn't included here already, you first have to include its header file, and then
@@ -73,9 +73,9 @@ constexpr uint8_t NUM_SCALES = 3;
 ScaleStorage scaleContainer = 
 {
   {
-    &scale_EbPentatonicMinor,   
-    &scale_CLydian,             
-    &scale_CPentatonicMajor     
+    &scale_EbPentatonicMinor,            // options: any of the scales defined in MusicalScales.h (default scale_EbPentatonicMinor)
+    &scale_CLydian,                      // options: any of the scales defined in MusicalScales.h (default scale_CLydian)
+    &scale_CPentatonicMajor              // options: any of the scales defined in MusicalScales.h (default scale_CPentatonicMajor)
   }, 
   0 // index of currently selected scale.
 };
@@ -85,24 +85,25 @@ ScaleStorage scaleContainer =
 // ambienceGenerator() specific configurations:
 // **********************************************************************************
 
-constexpr uint8_t RANDOMNESS_THRESHOLD = 255;     
+// Decrease this value to increase the probability of events that are partially randomly triggered
+constexpr uint8_t RANDOMNESS_THRESHOLD = 255;          // range 0 to 255 (255 default)
 
 // these mappings determine which color sensor channels affect various parameters in ambienceGenerator()
-#define OCTAVE_SHIFTER_CHANNEL        mappedWhite
-#define BASE_INTERVAL_CHANNEL         mappedWhite
+#define OCTAVE_SHIFTER_CHANNEL        mappedWhite      // options: mappedWhite, mappedRed, mappedGreen, mappedBlue (default mappedWhite)
+#define BASE_INTERVAL_CHANNEL         mappedWhite      // options: mappedWhite, mappedRed, mappedGreen, mappedBlue (default mappedWhite)     
 
-#define OSC_0_ARP_TRIGGER_CHANNEL     mappedRed
-#define OSC_0_NOTE_SELECTOR_CHANNEL   mappedGreen
-#define OSC_0_BUTTON_2_MODE_TIMER     mappedGreen
+#define OSC_0_ARP_TRIGGER_CHANNEL     mappedRed        // options: mappedWhite, mappedRed, mappedGreen, mappedBlue (default mappedRed)
+#define OSC_0_NOTE_SELECTOR_CHANNEL   mappedGreen      // options: mappedWhite, mappedRed, mappedGreen, mappedBlue (default mappedGreen)
+#define OSC_0_BUTTON_2_MODE_TIMER     mappedGreen      // options: mappedWhite, mappedRed, mappedGreen, mappedBlue (default mappedGreen)
 
-#define OSC_1_ARP_TRIGGER_CHANNEL     mappedGreen
-#define OSC_1_NOTE_SELECTOR_CHANNEL   mappedBlue 
-#define OSC_1_BUTTON_2_MODE_TIMER     mappedBlue 
+#define OSC_1_ARP_TRIGGER_CHANNEL     mappedGreen      // options: mappedWhite, mappedRed, mappedGreen, mappedBlue (default mappedGreen)
+#define OSC_1_NOTE_SELECTOR_CHANNEL   mappedBlue       // options: mappedWhite, mappedRed, mappedGreen, mappedBlue (default mappedBlue)
+#define OSC_1_BUTTON_2_MODE_TIMER     mappedBlue       // options: mappedWhite, mappedRed, mappedGreen, mappedBlue (default mappedBlue)
 
-#define OSC_2_ARP_TRIGGER_CHANNEL     mappedBlue 
-#define OSC_2_NOTE_SELECTOR_CHANNEL   mappedRed
-#define OSC_2_BUTTON_2_MODE_TIMER     mappedRed
+#define OSC_2_ARP_TRIGGER_CHANNEL     mappedBlue       // options: mappedWhite, mappedRed, mappedGreen, mappedBlue (default mappedBlue)
+#define OSC_2_NOTE_SELECTOR_CHANNEL   mappedRed        // options: mappedWhite, mappedRed, mappedGreen, mappedBlue (default mappedRed)
+#define OSC_2_BUTTON_2_MODE_TIMER     mappedRed        // options: mappedWhite, mappedRed, mappedGreen, mappedBlue (default mappedRed)
 
-#define GLOBAL_ARP_TRIGGER            mappedWhite
+#define GLOBAL_ARP_TRIGGER            mappedWhite      // options: mappedWhite, mappedRed, mappedGreen, mappedBlue (default mappedWhite)
 
 #endif
